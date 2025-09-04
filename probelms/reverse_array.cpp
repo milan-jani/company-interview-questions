1944a546514eabed9e9d02cfb038a6020ff709b3
milan-jani <janimilan7077@gmail.com>
2025-09-05 00:08:27 +0530

    solved reverse  the array

---
+++ probelms/reverse_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef vector<int> vii;
+
+void rev(vii &arr){
+    int n=arr.size();
+    for(int i=0;i<n/2;i++){
+        swap(arr[i],arr[n-i-1]);
+    }
+}
+int main(){ 
+    vii arr={1,2,4,5,3,6,8,7};
+    cout<<"original array:"<<endl;
+    for(int i:arr) cout<<i<<" ";
+    cout<<"\nReversed  array:"<<endl;
+    rev(arr);
+    for(int i:arr) cout<<i<<" ";
+}
--- probelms/sec_max-min.cpp
+++ probelms/sec_max-min.cpp
@@ -6,7 +6,7 @@ typedef pair<int,int> pii;
 
 
 pii sec_minimum_maximum(vi arr){ 
-  int ma=arr[0],mi=arr[0],smax=0,smin=INT_MAX;
+  int ma=arr[0],mi=arr[0],smax=INT_MIN,smin=INT_MAX;
   for(int i=1;i<arr.size();i++){
     int x=arr[i];
     if(x> ma){
@@ -19,6 +19,7 @@ pii sec_minimum_maximum(vi arr){
     }
     else if( x<smin && x > mi ) smin=x;
   }
+  if(smin==INT_MAX || smax==INT_MIN) return {-1,-1};
   return {smin,smax};
 }
  
@@ -26,9 +27,13 @@ int main(){
     //vi arr={1,2,4,7,7,5}; //1
    //vi arr={9,1,1,1,1,9,2,2,3,5,5};
     vi arr={7,9,21,12,72,4,12,5};
+    vi arr1={1};
 
    pii ans= sec_minimum_maximum(arr);
    cout<<"second minimum:"<<ans.first<<endl<<"Second largest:"<<ans.second<<endl;
+   ans= sec_minimum_maximum(arr1);
+   cout<<"second minimum:"<<ans.first<<endl<<"Second largest:"<<ans.second<<endl;
+
      return 0;
    
    
